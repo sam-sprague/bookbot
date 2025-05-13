2730dc9ad8b1cfbf16857cfa8f48534f9b22192c
sam-sprague <samuel.e.sprague@gmail.com>
2025-05-13 19:22:58 -0400

    updated main.py to print text

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+main()
