6a268528583ea03ef82bc4175cac561711230a9f
sam-sprague <samuel.e.sprague@gmail.com>
2025-05-13 19:27:39 -0400

    updated main.py to count words

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def get_book_text(path):
         return f.read()
 
 
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
 
 main()
