1725925251e4377e2086c1fa353fe2c8dbd88b03
sam-sprague <samuel.e.sprague@gmail.com>
2025-05-13 23:25:30 -0400

    updated main.py to work with any book

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 from stats import (
     get_num_words,
     get_chars_dict,
     chars_dict_to_sorted_list
 )
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,7 +24,10 @@ def print_report(book_path, num_words, chars_sorted_list):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
